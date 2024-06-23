d7cbda3a2db7d225dfb7143ce99e0c9a479a5754
SanyaSho <68691958+sanyasho@users.noreply.github.com>
2024-06-23 23:36:19 +0300

    game: client: restore original c_portal2_lesson

--- src/game/client/portal2/c_portal2_lesson.cpp
+++ src/game/client/portal2/c_portal2_lesson.cpp
@@ -1,31 +1,107 @@
-//========= Copyright © 1996-2008, Valve Corporation, All rights reserved. ============//
+//========= Copyright 1996-2008, Valve Corporation, All rights reserved. ============//
 //
-// Purpose:		Client handler implementations for instruction players how to play
+// Purpose:		Stub for custom mod lesson actions. 
+//				This is so that mods can do actions 
+//				Remove this file from the mod's vpc and include your own.
 //
 //=============================================================================//
 
 #include "cbase.h"
 
+#include "hud_closecaption.h"
 #include "c_gameinstructor.h"
 #include "c_baselesson.h"
+#include "ienginevgui.h"
+#include "message.h"
+#include "portal_mp_gamerules.h"
 
 // memdbgon must be the last include file in a .cpp file!!!
 #include "tier0/memdbgon.h"
 
 
-void CScriptedIconLesson::Mod_PreReadLessonsFromFile(void)
-{
+extern ConVar gameinstructor_verbose;
 
+
+bool IsListeningToCommentary( void );
+
+
+enum Mod_LessonAction
+{
+	// Enum starts from end of LessonAction
+	LESSON_ACTION_MOD_CUSTOM_ACTION_STUB = LESSON_ACTION_MOD_START,
+	
+	LESSON_ACTION_TOTAL
+};
+
+
+void CScriptedIconLesson::Mod_PreReadLessonsFromFile( void )
+{
+	// Add custom actions to the map
+	CScriptedIconLesson::LessonActionMap.Insert( "custom action stub", LESSON_ACTION_MOD_CUSTOM_ACTION_STUB );
 }
 
 
-bool CScriptedIconLesson::Mod_ProcessElementAction(int iAction, bool bNot, const char* pchVarName, EHANDLE& hVar, const CGameInstructorSymbol* pchParamName, float fParam, C_BaseEntity* pParam, const char* pchParam, bool& bModHandled)
+bool CScriptedIconLesson::Mod_ProcessElementAction( int iAction, bool bNot, const char *pchVarName, EHANDLE &hVar, const CGameInstructorSymbol *pchParamName, float fParam, C_BaseEntity *pParam, const char *pchParam, bool &bModHandled )
 {
+	// Assume we're going to handle the action
+	bModHandled = true;
+
+	C_BaseEntity *pVar;
+	pVar = hVar.Get();
+
+	switch ( iAction )
+	{
+		case LESSON_ACTION_MOD_CUSTOM_ACTION_STUB:
+		{
+			float flStub = 0.0f; //pVar->GetStubValue();
+
+			if ( gameinstructor_verbose.GetInt() > 0 && ShouldShowSpew() )
+			{
+				ConColorMsg( CBaseLesson::m_rgbaVerbosePlain, "\t[%s]->GetStubValue() ", pchVarName );
+				ConColorMsg( CBaseLesson::m_rgbaVerboseName, "%.1f ", flStub );
+				ConColorMsg( CBaseLesson::m_rgbaVerbosePlain, ( bNot ) ? ( ">= [%s] " ) : ( "< [%s] " ), pchParamName->String() );
+				ConColorMsg( CBaseLesson::m_rgbaVerboseName, "%.1f\n", fParam );
+			}
+
+			return ( flStub ) ? ( flStub >= fParam ) : ( flStub < fParam );
+		}
+
+		default:
+			// Didn't handle this action
+			bModHandled = false;
+			break;
+	}
+
 	return false;
 }
 
+
 bool C_GameInstructor::Mod_HiddenByOtherElements( void )
 {
+	if ( engine->IsPaused() || enginevgui->IsGameUIVisible() )
+	{
+		return true;
+	}
+
+	CHudMessage *pHudMessage = GET_HUDELEMENT( CHudMessage );
+	if ( pHudMessage && pHudMessage->ShouldDraw() )
+	{
+		return true;
+	}
+
+	if ( IsListeningToCommentary() )
+	{
+		CHudCloseCaption *pHudCloseCaption = (CHudCloseCaption *)GET_FULLSCREEN_HUDELEMENT( CHudCloseCaption );
+		if ( pHudCloseCaption && pHudCloseCaption->ShouldDraw() && pHudCloseCaption->IsVisible() )
+		{
+			return true;
+		}
+	}
+
+	if ( PortalMPGameRules() && PortalMPGameRules()->IsCreditsMap() )
+	{
+		return true;
+	}
 
 	return false;
 }
